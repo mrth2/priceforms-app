2912dac38246fe86ac1f12b8d1e25feb6b4bf789
mrth2 <mr.th2.90@gmail.com>
2022-03-06 14:57:57 +0700

    setup tailwindcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,21 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/strapi'],
+  css: [
+    '@/assets/css/main.css',
+  ],
+  buildModules: [
+    '@nuxtjs/strapi',
+  ],
   strapi: {
     url: process.env.API_URL || 'http://localhost:1337',
     prefix: '/api',
     version: 'v4',
     cookie: {}
+  },
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    }
   }
 })
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="text-center text-3xl font-bold mt-20">
+  <h1>This is the Form page</h1>
+</div>
+</template>
