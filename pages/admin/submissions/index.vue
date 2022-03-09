eb449f2350b69766b1de338527cf78c1b22318d3
mrth2 <mr.th2.90@gmail.com>
2022-03-09 15:30:22 +0700

    add date formater & load submission data to table

--- pages/admin/submissions/index.vue
+++ pages/admin/submissions/index.vue
@@ -1,127 +1,129 @@
 <script setup lang="ts">
-import { IUser } from '~~/types/user';
-
 definePageMeta({
-  layout: 'admin',
-  title: 'Submissions',
-})
-const { find } = useStrapi4()
+  layout: "admin",
+  title: "Submissions",
+});
+const { find } = useStrapi4();
 
-const { data } = await useAsyncData('form-submissions', () => find<IUser>('form-submissions'))
-console.log(data.value)
-
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-]
+const { data } = await useAsyncData("form-submissions", () =>
+  find<{ data: any }>("form-submissions", {
+    fields: [
+      "status",
+      "progress",
+      "stopAt",
+      "minPrice",
+      "maxPrice",
+      "currency",
+      "createdAt",
+      "updatedAt",
+    ],
+    populate: ["form", "category", "user"],
+  })
+);
+const submissions = computed(() =>
+  data.value.data.map((s) => ({
+    id: s.id,
+    ...s.attributes,
+    category: {
+      id: s.attributes.category.data.id,
+      ...s.attributes.category.data.attributes,
+    },
+    form: {
+      id: s.attributes.form.data.id,
+      ...s.attributes.form.data.attributes,
+    },
+    user: {
+      id: s.attributes.user.data.id,
+      ...s.attributes.user.data.attributes,
+    },
+  }))
+);
+console.log(submissions.value);
 </script>
 
 <template>
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <div
+          class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+        >
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
-                <th
-                  scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >Name</th>
-                <th
-                  scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >Title</th>
-                <th
-                  scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >Status</th>
-                <th
-                  scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >Role</th>
+                <th scope="col" class="heading">Name</th>
+                <th scope="col" class="heading">Form</th>
+                <th scope="col" class="heading">Status</th>
+                <th scope="col" class="heading">Progress</th>
+                <th scope="col" class="heading">Stoped At</th>
+                <th scope="col" class="heading">Price</th>
+                <th scope="col" class="heading">Last Updated</th>
                 <th scope="col" class="relative px-6 py-3">
                   <span class="sr-only">Edit</span>
                 </th>
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="person in people" :key="person.email">
-                <td class="px-6 py-4 whitespace-nowrap">
+              <tr v-for="item in submissions" :key="item.id">
+                <td class="row">
                   <div class="flex items-center">
                     <div class="flex-shrink-0 h-10 w-10">
-                      <img class="h-10 w-10 rounded-full" :src="person.image" alt="user" />
+                      <div
+                        class="h-10 w-10 rounded-full border border-gray-300 bg-blue-100 flex items-center justify-center font-medium tracking-tight"
+                        alt="user"
+                      >
+                        {{ item.user.firstName.charAt(0) }}
+                        {{ item.user.lastName.charAt(0) }}
+                      </div>
                     </div>
                     <div class="ml-4">
-                      <div class="text-sm font-medium text-gray-900">{{ person.name }}</div>
-                      <div class="text-sm text-gray-500">{{ person.email }}</div>
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ item.user.firstName }}
+                        {{ item.user.lastName }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ item.user.email }}
+                      </div>
                     </div>
                   </div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-900">{{ person.title }}</div>
-                  <div class="text-sm text-gray-500">{{ person.department }}</div>
+                <td class="row">
+                  <div class="text-sm text-gray-900 uppercase">
+                    {{ item.form.subDomain }}
+                  </div>
+                  <div class="text-sm text-gray-500">
+                    Case: {{ item.category.title }}
+                  </div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
+                <td class="row">
                   <span
-                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
-                  >Active</span>
+                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full capitalize"
+                    :class="{
+                      'bg-cyan-100 text-cyan-800': item.status === 'register',
+                      'bg-blue-100 text-blue-800': item.status === 'partial',
+                      'bg-green-100 text-green-800':
+                        item.status === 'completed',
+                    }"
+                  >
+                    {{ item.status }}
+                  </span>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ person.role }}</td>
-                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                <td class="row">{{ item.progress }}%</td>
+                <td class="row !text-[10px] !whitespace-normal w-16 capitalize">
+                  {{ item.stopAt.toLowerCase() || "Just signed up" }}
+                </td>
+                <td class="row text-sm">
+                  {{ item.minPrice }} ~ {{ item.maxPrice }} ({{
+                    item.currency
+                  }})
+                </td>
+                <td class="row">
+                  {{ $dateFormat(item.updatedAt) }}
+                </td>
+                <td class="row text-right text-sm font-medium">
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                    Edit
+                  </a>
                 </td>
               </tr>
             </tbody>
@@ -131,3 +133,12 @@ const people = [
     </div>
   </div>
 </template>
+
+<style scoped lang="postcss">
+.heading {
+  @apply px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-max whitespace-nowrap;
+}
+.row {
+  @apply px-6 py-4 whitespace-nowrap text-sm text-gray-500;
+}
+</style>
---
+++ plugins/date-format.ts
@@ -0,0 +1,32 @@
+export default defineNuxtPlugin(nuxtApp => {
+  return {
+    provide: {
+      dateFormat: (rawTime: string) => {
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        // let date = new Date("2021-10-30T19:51:27.710Z");
+        let date = new Date(rawTime);
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let day = date.getDate();
+        let hh = date.getHours();
+        let mm = date.getMinutes();
+    
+        let AmOrPm = hh >= 12 ? 'AM' : 'AM';
+        hh = (hh % 12) || 12;
+    
+        let diffInSec = Math.floor((Date.now() - date.getTime()) / 1000);
+        if (diffInSec < 30) {
+          return 'Just Now';
+        }
+        if (diffInSec < 59) {
+          return diffInSec + ' seconds ago';
+        }
+        let diffInMin = Math.floor(diffInSec / 60);
+        if (diffInMin < 59) {
+          return diffInMin + ' mins ago';
+        }
+        return months[month] + ' ' + day + ' ' + year + ' , ' + hh + ':' + mm + ' ' + AmOrPm;
+      }
+    }
+  }
+});
