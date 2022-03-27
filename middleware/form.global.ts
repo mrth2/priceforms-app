fb1c0d700e3e21e0a6b7c728797053c74f473060
mrth2 <mr.th2.90@gmail.com>
2022-03-27 17:24:13 +0700

    fix form loading missing on admin

--- middleware/form.global.ts
+++ middleware/form.global.ts
@@ -3,13 +3,18 @@ import { useSubmissionStore } from "~~/store/submission";
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const is404 = to.name === "404";
-  const isOnAdmin = to.path.includes('/admin');
-  if (is404 || isOnAdmin) return;
+  if (is404) return;
   // always fetch forms & flows
   const formStore = useFormStore();
   const requests = [
     formStore.loadForm(),
   ];
+  const isOnAdmin = to.path.includes('/admin');
+  // on admin, load only the forms
+  if (isOnAdmin) {
+    await Promise.all(requests);
+    return;
+  }
   const requireCategories = to.path.includes('/cases');
   const requireLoadingFlows = to.path.includes('/question') || to.path.includes('/cases');
   if (requireCategories) {
